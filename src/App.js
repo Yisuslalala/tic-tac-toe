9edd3b649effd54d648c7e577e5c54cec758c1f5
Jesús Quiñones <1930318@upv.edu.mx>
2023-07-19 00:08:43 -0600

    Alost finish UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import './App.css';
 
 function Square({ value, onSquareClick }) {
   return (
@@ -36,24 +36,35 @@ export default function Board() {
   }
   
   return (
-    <>
-    <div className='status'>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
+    <div className='App'>
+      <header>Tic-tac-toe</header>
+      <div className='status'>{status}</div>
+        <div className='game'>
+          <div className="board-row">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
+        </div>
+        <footer>
+          <ul>
+            <li><a href="https://twitter.com/Yisuslalala">Twitter</a></li>
+            <li><a href="https://github.com/Yisuslalala">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/jes%C3%BAs-qui%C3%B1ones/">Linkedin</a></li>
+            <li><a href="https://www.youtube.com/channel/UCZ3Q1amj-UR83oBNNwn7rkA">Youtube</a></li>
+          </ul>
+        </footer>
+    </div>
   );
 }
 
